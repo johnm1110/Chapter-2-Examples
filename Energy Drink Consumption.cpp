7f0c91599e4978cd9ab117a8463bf63593bb4724
johnm1110 <jamiller1110@gmail.com>
2024-09-16 20:10:46 -0400

    removed old example

--- Energy Drink Consumption.cpp
+++ Energy Drink Consumption.cpp
@@ -14,7 +14,7 @@ int main()
 	float preferCitris = customers * citrisPercent;
 
 	// Display the results
-	cout << “The number of customers who buy one or more energy drinks per week is “ << oneOrMore << "/n";
-	cout << “The number of customers who prefer citris flaovered energy drinks is” << preferCitris << "/n";
+	cout << "The number of customers who buy one or more energy drinks per week is " << oneOrMore << " \n";
+	cout << "The number of customers who prefer citris flaovered energy drinks is " << preferCitris << " \n";
 	return 0;
 }
